e38ba116a369aa82715f2358954aab3ef6733898
matchaghost <tiffany.meldau@gmail.com>
2021-02-07 12:00:41 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,13 @@
+let quotes= [
+    `"Do or do not, there is no try."
+-Yoda`,
+    `"Life finds a way"
+-Ian Malcolm`,
+    `"Friends don't lie"
+-Eleven`
+
+]
+
+
+let num = Math.floor(Math.random()*3);
+console.log(quotes[num]);
